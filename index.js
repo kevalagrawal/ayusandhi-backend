b9eb1db8124022891de7663beaac7adc0d27d9af
Keval Agrawal <145700369+kevalagrawal@users.noreply.github.com>
2025-10-09 22:42:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ app.get("/health", (_req, res) => {
 	res.json({ status: "ok" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
